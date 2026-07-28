a9d9f014d9c25a5fe6c4f1d49e4e5676d8e2efaa
wernerbosch40-glitch <wernerbosch40@gmail.com>
2026-07-28 16:41:54 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = Object.freeze({
+  SUPABASE_URL: "https://hfnqmkzkrmfqbsgpfgzg.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_7D6DV01nqROwu_60mTbAig_JEtjdjTF",
+  LEADS_TABLE: "leads",
+  WHATSAPP_NUMBER: "27823971778",
+  AGENT_NAME: "Werner Bosch"
+});
